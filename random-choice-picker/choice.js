ae828843c85f9edf3849375cb72547c5c35eb95a
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-01-23 03:57:37 +0530

    generating random choice from computer

--- random-choice-picker/choice.js
+++ random-choice-picker/choice.js
@@ -1,6 +1,6 @@
 let choiceCount;
 let htmlDisplayInput ='';
-
+let compChoice;
 
 let displayInput = document.querySelector('.js-gen-input');
 let inputCountElem = document.querySelector('.js-choice-count');
@@ -12,9 +12,19 @@ document.querySelector('.js-next-btn')
         for(let i=1; i<=choiceCount;i++){
             htmlDisplayInput += `<p><input type="text" class=id-${i}></p>`
         }
-        console.log(htmlDisplayInput);
         displayInput.innerHTML = htmlDisplayInput;
+
+        compChoice = Math.floor(Math.random() * (choiceCount)) + 1;
     });
+
+
+let compChoiceInput = document.querySelector(`.id-${compChoice}`); 
+
+document.querySelector('.js-gen-choice')
+    .addEventListener('click',()=>{
+        console.log(compChoiceInput.value);
+    })
+
 
 document.querySelector('.js-reset')
     .addEventListener('click',()=>{
