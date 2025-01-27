916181065516158ae911bfd741907ac81de820a8
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-01-28 03:13:00 +0530

    adding number's image for easy understanding the logic of button's click

--- image-slider/slide.js
+++ image-slider/slide.js
@@ -1,16 +1,32 @@
+// let imageArray = [
+//     'images/sung.webp',
+//     'images/sasuke.webp',
+//     'images/naruto.webp',
+//     'images/luffy.webp',
+//     'images/girl2.webp',
+//     'images/girl.webp',
+//     'images/aeroplane.webp'
+// ];
+
+
 let imageArray = [
-    'images/sung.webp',
-    'images/sasuke.webp',
-    // 'images/naruto.webp',
-    // 'images/luffy.webp',
-    // 'images/girl2.webp',
-    // 'images/girl.webp',
-    // 'images/aeroplane.webp'
-];
+    'images/number-image/one.webp',
+    'images/number-image/two.webp',
+    'images/number-image/three.webp',
+    'images/number-image/four.webp',
+    'images/number-image/five.jpg',
+    'images/number-image/six.webp',
+    'images/number-image/seven.webp',
+    'images/number-image/eight.webp',
+    'images/number-image/nine.jpg',
+    'images/number-image/ten.webp',
+]
+
 
 let i = 0;
 
 let imageDisplay = document.querySelector('.js-image-holder');
+// let preBtn = document.querySelector('.js-pre');
 
 setInterval(() => {
     imageDisplay.innerHTML = `<img src="${imageArray[i]}" class="css-imgDisplay">`;
@@ -19,5 +35,14 @@ setInterval(() => {
     if (i >= imageArray.length) {
         i=0;
     }
-}, 3000);
+}, 5000);
 
+
+document.querySelector('.js-pre').addEventListener('click',()=>{
+    i--;
+    console.log(i);
+});
+
+document.querySelector('.js-nxt').addEventListener('click',()=>{
+    console.log('next');
+});
